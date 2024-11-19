bdfcc03cce15671bb5b11f74ef8553c2bad83203
Sina Mohammadi <160613931+SinaAce@users.noreply.github.com>
2024-11-20 02:16:21 +0330

    configUpdate

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // این تنظیم به Vite اجازه می‌دهد تا از هر آدرس شبکه‌ای قابل دسترسی باشد
+    port: 5173,  // پورت 5173 به طور پیش‌فرض است (می‌توانید هر پورت دیگری انتخاب کنید)
+  }
 })
