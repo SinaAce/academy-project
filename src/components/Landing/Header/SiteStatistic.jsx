6b4365adfaaea5f6e37074e2253a67bb9ade6a7d
Sina Mohammadi <160613931+SinaAce@users.noreply.github.com>
2024-12-18 00:16:42 +0330

    site-statistic api is done

--- src/components/Landing/Header/SiteStatistic.jsx
+++ src/components/Landing/Header/SiteStatistic.jsx
@@ -1,21 +1,52 @@
-import { useState } from "react";
-import fakeData from "../../../data/fake-api.json";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import teacherIcon from "../../../assets/Landing/header/Icon.svg";
+import courseIcon from "../../../assets/Landing/header/Icon2.svg";
+import studentIcon from "../../../assets/Landing/header/Icon3.svg";
 
 const SiteStatistic = () => {
-  const [data, setData] = useState(fakeData);
+  const [siteStatic, setSiteStatic] = useState([]); // مقدار اولیه آرایه
 
+  const getSiteStatic = async () => {
+    const res = await axios.get(
+      "https://classapi.sepehracademy.ir/api/Home/LandingReport"
+    );
+    setSiteStatic(res.data);
+  };
+
+  useEffect(() => {
+    getSiteStatic();
+    console.log(siteStatic);
+  }, []);
   return (
     <div className="w-[724px] h-[211px] z-20 flexItemCenter gap-8">
-      {data.map((e) => (
-        <div
-          key={e.id}
-          className="w-1/3 h-full backdrop-blur-md border-4 border-white rounded-2xl flexItemCol"
-        >
-          <img className="cursor-pointer" src={e.image} alt={e.title} />
-          <span className="text-[2.25em] font-black">{e.number}</span>
-          <span className="text-[#455A64] text-[1.25em]">{e.title}</span>
-        </div>
-      ))}
+      <div className="w-1/3 h-full backdrop-blur-md border-4 border-white rounded-2xl flexItemCol gap-2">
+        <img src={studentIcon} alt="teacherIcon" />
+        <span className="text-[#455A64] text-[2.25em] font-black">
+          {siteStatic.teacherCount}
+        </span>
+        <span className="text-[1.25em] text-[#263238] font-bold">
+          مدرس مجرب
+        </span>
+      </div>
+      <div className="w-1/3 h-full backdrop-blur-md border-4 border-white rounded-2xl flexItemCol gap-2">
+        <img src={teacherIcon} alt="teacherIcon" />
+        <span className="text-[#455A64] text-[2.25em] font-black">
+          {siteStatic.studentCount}
+        </span>
+        <span className="text-[1.25em] text-[#263238] font-bold">
+          نفر دانشجو
+        </span>
+      </div>
+      <div className="w-1/3 h-full backdrop-blur-md border-4 border-white rounded-2xl flexItemCol gap-2">
+        <img src={courseIcon} alt="teacherIcon" />
+        <span className="text-[#455A64] text-[2.25em] font-black">
+          {siteStatic.courseCount}
+        </span>
+        <span className="text-[1.25em] text-[#263238] font-bold">
+          دوره فعال{" "}
+        </span>
+      </div>
     </div>
   );
 };
