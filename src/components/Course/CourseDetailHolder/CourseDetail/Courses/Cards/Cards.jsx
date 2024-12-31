8786a82c142931e294e14668707ca4ec87723f4d
Sina Mohammadi <160613931+SinaAce@users.noreply.github.com>
2024-12-31 16:38:59 +0330

    jalali moment

--- src/components/Course/CourseDetailHolder/CourseDetail/Courses/Cards/Cards.jsx
+++ src/components/Course/CourseDetailHolder/CourseDetail/Courses/Cards/Cards.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import moment from "jalali-moment";
+import UnImg from "../../../../../../assets/courses/Unimages.png";
+import note from "../../../../../../assets/courses/note-2.svg";
+import clock from "../../../../../../assets/courses/clock.svg";
+import calender from "../../../../../../assets/courses/calendar-2.svg";
 
 const Cards = () => {
   const [courses, setCourses] = useState([]);
@@ -7,7 +12,7 @@ const Cards = () => {
   const getCourseList = async () => {
     try {
       const res = await axios.get(
-        "https://classapi.sepehracademy.ir/api/Home/GetCoursesWithPagination?PageNumber=1&RowsOfPage=10&SortingCol=Active&SortType=DESC&TechCount=0"
+        "https://classapi.sepehracademy.ir/api/Home/GetCoursesWithPagination?PageNumber=1&RowsOfPage=20&SortingCol=Active&SortType=DESC&TechCount=0"
       );
       setCourses(res.data.courseFilterDtos);
     } catch (error) {
@@ -17,8 +22,13 @@ const Cards = () => {
 
   useEffect(() => {
     getCourseList();
-    console.log(courses);
   }, []);
+
+  const convertToFullJalaliDate = (gregorianDate) => {
+    return moment(gregorianDate, "YYYY-MM-DDTHH:mm:ss")
+      .locale("fa")
+      .format("D MMMM YYYY");
+  };
 
   return (
     <div className="flex flex-wrap gap-4">
@@ -30,19 +40,28 @@ const Cards = () => {
           >
             <div className="w-full h-3/6 rounded-[20px]">
               <img
-                src={e.tumbImageAddress || "placeholder.jpg"}
+                src={e.tumbImageAddress || UnImg}
                 alt="coursePicture"
                 className="w-full h-full object-cover rounded-[20px]"
               />
             </div>
             <div className="w-full h-3/5 mt-1 flexItemCol">
               <div className="w-full h-1/4 text-base font-bold">
-                {e.courseName || "نام دوره در دسترس نیست"}
+                {e.title || "نام دوره در دسترس نیست"}
               </div>
               <div className="w-full bg-[#ECEFF1] rounded-3xl h-1/4 flexItemAround">
-                <div className="h-full border border-red-500 w-1/4 flex"></div>
-                <div className="h-full border border-red-500 w-1/4 flex"></div>
-                <div className="h-full border border-red-500 w-1/4 flex"></div>
+                <div className="h-full flexItem gap-1 text-xs font-medium text-right">
+                  <img src={note} alt="icon" />
+                  {e.levelName}
+                </div>
+                <div className="h-full flexItem gap-1 text-xs font-medium text-right">
+                  <img src={clock} alt="icon" />
+                  {e.commandCount} ساعت آموزش
+                </div>
+                <div className="h-full flexItem gap-1 text-xs font-medium text-right">
+                  <img src={calender} alt="icon" />
+                  {convertToFullJalaliDate(e.lastUpdate)}
+                </div>
               </div>
               <div className="w-full bg-[#ECEFF1] rounded-3xl h-1/4"></div>
               <div className="w-full bg-[#ECEFF1] rounded-3xl h-1/4"></div>
